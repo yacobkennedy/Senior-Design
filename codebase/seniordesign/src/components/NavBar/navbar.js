b02a9ad205ef1abbd7ea637fc5d3cfbbfc4aeaf1
Jacob Kennedy <yaypa.kennedy@gmail.com>
2024-03-27 11:12:03 -0400

    Updated project

--- codebase/seniordesign/src/components/NavBar/navbar.js
+++ codebase/seniordesign/src/components/NavBar/navbar.js
@@ -1,27 +1,64 @@
 import { useState, useEffect } from 'react';
-import styles from './navbar.module.css'
+import styles from './navbar.module.css';
 import { useNavigate, useLocation } from "react-router-dom";
 import { Outlet, Link } from "react-router-dom";
+import goodplaces from '../../images/goodplaces.png';
 
 function NavBar() {
-    const navigate = useNavigate();
+  // Code to initially set visible sessionstorage value but not reset it every time component is rerendered
+  useEffect(() => {
+    // Check if the value exists in sessionStorage
+    const storedValue = sessionStorage.getItem('loginvisible');
 
-  // Handle the button click to navigate to the Signup page
+    // If the value doesn't exist, set it
+    if (!storedValue) {
+      sessionStorage.setItem('loginvisible', JSON.stringify(true));
+      console.log('Value set in sessionStorage');
+      setIsVisible(true)
+    } else {
+      console.log('Value already exists in sessionStorage');
+    }
+  }, []);
+
+  // State to manage if Login button should be visible or replaced with the profile link
+  const [isVisible, setIsVisible] = useState(JSON.parse(sessionStorage.getItem('loginvisible')))
+  console.log('visibility', isVisible)
+
+  const navigate = useNavigate();
+
+  // Handle the button click to navigate to the Login page
   const useLogin = () => {
     navigate("/login");
   }
 
+  // Function for handling profile options after login
+  const profileDropdown = () => {
 
-    return (
-        <div className={styles.navbar}>
+  }
 
-            <div className={styles.loginContainer}>
-                
-                <button className={styles.button} onClick={useLogin}> Login </button>
-            </div>
-            
-        </div>
-    )
+
+  return (
+      <div className={styles.navbar}>
+
+          <div className={styles.loginContainer}>
+              
+              {isVisible && <button className={styles.button} onClick={useLogin}> Login </button>}
+
+              {!isVisible && <select> Profile </select>}
+          </div>
+
+          <div className={styles.logoContainer}>
+            <Link to="/">
+              <img className={styles.logo} src={goodplaces} alt="logo image"/>
+              </Link>
+            <Link to="/" style={{ textDecoration: "none"}}>
+              <p className={styles.logoText}> <b>Goodplaces</b></p>
+              </Link>
+
+          </div>
+          
+      </div>
+  )
 }
 
 export default NavBar;
--- codebase/seniordesign/src/pages/Home.js
+++ codebase/seniordesign/src/pages/Home.js
@@ -1,14 +1,26 @@
 import { useState, useEffect } from 'react';
 import styles from './Home.module.css'
 import NavBar from '../components/NavBar/navbar';
+import Selection from '../components/Selection/selection';
+
+
+// Make dynamic pages off of selections from here, once localstorage place value gets set we can request that data from the api and make selections from there
 
 
 function Home() {
+
     return(
         <div className={styles.homePage}>
             <NavBar/>
             <div className={styles.homeInfo}>
-                
+
+                <div className={styles.header}>
+                    <p> <b>Discover. Explore. Adventure.</b> </p>
+                </div>
+
+                <div className={styles.selectionContainer}>
+                    <Selection/>
+                </div>
             </div>
         </div>
     )
